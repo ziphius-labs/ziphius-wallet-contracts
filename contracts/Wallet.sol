ffb235cf3dd72277e85da59c06681ecb978751ac
Terry <duchuy.124dk@gmail.com>
2023-05-27 22:24:21 +0700

    style: fix lint

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -51,7 +51,6 @@ contract Wallet is CoreWallet, IWallet {
      * @dev See {IERC165-supportsInterface}.
      */
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IWallet).interfaceId
-            || super.supportsInterface(interfaceId);
+        return interfaceId == type(IWallet).interfaceId || super.supportsInterface(interfaceId);
     }
 }
